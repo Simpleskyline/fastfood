5a7cee63c7fdb54e5111e124308c45ccd958047a
Simpleskyline <telvintran@gmail.com>
2026-01-30 16:07:43 +0300

    Fix header overlay and cleanup CSS

--- backend/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "fastfood");
-if ($conn->connect_error) {
-    http_response_code(500);
-    echo json_encode(["success" => false, "message" => "Database connection failed"]);
-    exit;
-}
-return $conn;
-?>
--- documentation/README.md
+++
@@ -1,2 +0,0 @@
-This is a fast food website that allows users to select their orders and have it delivered to teir pinned location.
-Also payments are done in the website.
